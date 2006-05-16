e0b7bbbaf2e0d7e0ecf6d8e4971d644af5cff021
Simon Wistow <simon@thegestalt.org>
2006-05-16 15:53:25 +0000

    Start the refactor

--- lib/Module/Pluggable.pm
+++ lib/Module/Pluggable.pm
@@ -2,18 +2,34 @@ package Module::Pluggable;
 
 use strict;
 use vars qw($VERSION);
-use File::Find ();
-use File::Basename;
-use File::Spec::Functions qw(splitdir catdir abs2rel);
-use Carp qw(croak carp);
-
+use Module::Pluggable::Object;
 
 # ObQuote:
 # Bob Porter: Looks like you've been missing a lot of work lately. 
 # Peter Gibbons: I wouldn't say I've been missing it, Bob! 
 
 
-$VERSION = '2.98';
+$VERSION = '3.0';
+
+sub import {
+    my $class        = shift;
+    my %opts         = @_;
+
+	my ($package)    = caller;
+    # the default name for the method is 'plugins'
+    my $sub          = $opts{'sub_name'}  || 'plugins';
+    # get our package 
+    my ($pkg)        = $opts{'package'} || $package;
+
+	my $finder       = Module::Pluggable::Object->new(%opts);
+    my $subroutine   = sub { return $finder->plugins(@_) };
+
+    no strict 'refs';
+    no warnings 'redefine';
+    *{"$pkg\::$sub"} = $subroutine;
+}
+
+1;
 
 =pod
 
@@ -289,243 +305,3 @@ L<File::Spec>, L<File::Find>, L<File::Basename>, L<Class::Factory::Util>, L<Modu
 =cut 
 
 
-sub import {
-    my $class   = shift;
-    my %opts    = @_;
-
-    # override 'require'
-    $opts{'require'} = 1 if $opts{'inner'};
-
-    if ($opts{'par'}) {
-    
-    }
-
-    my ($package, $filename) = caller;
-
-
-    my $file_regex = $opts{'file_regex'} || qr/\.pm$/;
-
-    # automatically turn a scalar search path or namespace into a arrayref
-    for (qw(search_path search_dirs)) {
-        $opts{$_} = [ $opts{$_} ] if exists $opts{$_} && !ref($opts{$_});
-    }
-
-
-    # the default name for the method is 'plugins'
-    my $sub = $opts{'sub_name'} || 'plugins';
-  
-
-    # get our package 
-    my ($pkg) = $opts{'package'} || $package;
-
-    my $subroutine = sub {
-        my $self = shift;
-
-
-        # default search path is '<Module>::<Name>::Plugin'
-        $opts{'search_path'} = ["${pkg}::Plugin"] unless $opts{'search_path'}; 
-
-        # predeclare
-        my @plugins;
-
-        
-        # check to see if we're running under test
-        my @SEARCHDIR = exists $INC{"blib.pm"} && $filename =~ m!(^|/)blib/! ? grep {/blib/} @INC : @INC;
-
-        # add any search_dir params
-        unshift @SEARCHDIR, @{$opts{'search_dirs'}} if defined $opts{'search_dirs'};
-
-
-
-        # go through our @INC
-        foreach my $dir (@SEARCHDIR) {
-            # and each directory in our search path
-            foreach my $searchpath (@{$opts{'search_path'}}) {
-                # create the search directory in a cross platform goodness way
-                my $sp = catdir($dir, (split /::/, $searchpath));
-                # if it doesn't exist or it's not a dir then skip it
-                next unless ( -e $sp && -d _ ); # Use the cached stat the second time
-
-
-                # find all the .pm files in it
-                # this isn't perfect and won't find multiple plugins per file
-                #my $cwd = Cwd::getcwd;
-                my @files = ();
-                { # for the benefit of perl 5.6.1's Find, localize topic
-                  local $_;
-                  File::Find::find( { no_chdir => 1, wanted =>
-                      sub { # Inlined from File::Find::Rule C< name => '*.pm' >
-                          return unless $File::Find::name =~ /$file_regex/;
-                          (my $path = $File::Find::name) =~ s#^\\./##;
-                          push @files, $path;
-                      }},
-                      $sp );
-                }
-                #chdir $cwd;
-
-                # foreach one we've found 
-                foreach my $file (@files) {
-                    # untaint the file; accept .pm only
-                    next unless ($file) = ($file =~ /(.*$file_regex)$/); 
-                    # parse the file to get the name
-                    my ($name, $directory) = fileparse($file, $file_regex);
-
-                    $directory = abs2rel($directory, $sp);
-                    # then create the class name in a cross platform way
-                    $directory =~ s/^[a-z]://i if($^O =~ /MSWin32|dos/);       # remove volume
-                    if ($directory) {
-                      ($directory) = ($directory =~ /(.*)/);
-                    }
-                    else {
-                      $directory = "";
-                    }
-                    my $plugin = join "::", splitdir catdir($searchpath, $directory, $name);
-
-                    next unless $plugin =~ m!(?:[a-z\d]+)[a-z\d]!i;
- 
-                   if (defined $opts{'instantiate'} || $opts{'require'}) { 
-                        
-                        eval "CORE::require $plugin";
-                        carp "Couldn't require $plugin : $@" if $@;
-                    }
-                    push @plugins, $plugin;
-                }
-
-                # now add stuff that may have been in package
-                # NOTE we should probably use all the stuff we've been given already
-                # but then we can't unload it :(
-                unless (exists $opts{inner} && !$opts{inner}) {
-                    for (list_packages($searchpath)) {
-                        if (defined $opts{'instantiate'} || $opts{'require'}) {
-                            eval "CORE::require $_";
-                            # *No warnings here* 
-                            # next if $@;
-                        }    
-                        push @plugins, $_;
-                    } # for list packages
-                } # unless inner
-            } # foreach $searchpath
-        } # foreach $dir
-
-
-
-
-        # push @plugins, map { print STDERR "$_\n"; $_->require } list_packages($_) for (@{$opts{'search_path'}});
-        
-        # return blank unless we've found anything
-        return () unless @plugins;
-
-
-        # exceptions
-        my %only;   
-        my %except; 
-        my $only;
-        my $except;
-
-        if (defined $opts{'only'}) {
-            if (ref($opts{'only'}) eq 'ARRAY') {
-                %only   = map { $_ => 1 } @{$opts{'only'}};
-            } elsif (ref($opts{'only'}) eq 'Regexp') {
-                $only = $opts{'only'}
-            } elsif (ref($opts{'only'}) eq '') {
-                $only{$opts{'only'}} = 1;
-            }
-        }
-        
-
-        if (defined $opts{'except'}) {
-            if (ref($opts{'except'}) eq 'ARRAY') {
-                %except   = map { $_ => 1 } @{$opts{'except'}};
-            } elsif (ref($opts{'except'}) eq 'Regexp') {
-                $except = $opts{'except'}
-            } elsif (ref($opts{'except'}) eq '') {
-                $except{$opts{'except'}} = 1;
-            }
-        }
-
-
-
-
-
-
-        # remove duplicates
-        # probably not necessary but hey ho
-        my %plugins;
-        for(@plugins) {
-            next if (keys %only   && !$only{$_}     );
-            next unless (!defined $only || m!$only! );
-
-            next if (keys %except &&  $except{$_}   );
-            next if (defined $except &&  m!$except! );
-            $plugins{$_} = 1;
-        }
-
-        # are we instantiating or requring?
-        if (defined $opts{'instantiate'}) {
-            my $method = $opts{'instantiate'};
-            return map { ($_->can($method)) ? $_->$method(@_) : () } keys %plugins;
-        } else { 
-            # no? just return the names
-            return keys %plugins;
-        }
-
-
-    };
-
-
-    my $searchsub = sub {
-              my $self = shift;
-              my ($action,@paths) = @_;
-
-              $opts{'search_path'} = ["${pkg}::Plugin"] if ($action eq 'add'  and not   $opts{'search_path'} );; 
-              push @{$opts{'search_path'}}, @paths      if ($action eq 'add');
-              $opts{'search_path'}       = \@paths      if ($action eq 'new');
-              return $opts{'search_path'};
-    };
-
-    my $onlysub = sub {
-        my ($self, $only) = @_;
-
-        if (defined $only) {
-            $opts{'only'} = $only;
-        };
-        
-        return $opts{'only'};
-    };
-
-    my $exceptsub = sub {
-        my ($self, $except) = @_;
-
-        if (defined $except) {
-            $opts{'except'} = $except;
-        };
-        
-        return $opts{'except'};
-    };
-
-    no strict 'refs';
-    no warnings 'redefine';
-    *{"$pkg\::$sub"} = $subroutine;
-    *{"$pkg\::search_path"} = $searchsub;
-    *{"$pkg\::only"} = $onlysub;
-    *{"$pkg\::except"} = $exceptsub;
-}
-
-
-sub list_packages {
-            my $pack = shift; $pack .= "::" unless $pack =~ m!::$!;
-
-            no strict 'refs';
-            my @packs;
-            for (grep !/^(main|)::$/, grep /::$/, keys %{$pack})
-            {
-                s!::$!!;
-                my @children = list_packages($pack.$_);
-                push @packs, "$pack$_" unless @children or /^::/; 
-                push @packs, @children;
-            }
-            return grep {$_ !~ /::::ISA::CACHE/} @packs;
-}
-
-
-1;
