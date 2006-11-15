20bb8770381c333e788fd85a88f781ae4e942c38
Simon Wistow <simon@thegestalt.org>
2006-11-15 15:40:24 +0000

    Remove Class::Inspector dep

--- lib/Devel/InnerPackage.pm
+++ lib/Devel/InnerPackage.pm
@@ -4,9 +4,6 @@ use strict;
 use base qw(Exporter);
 use vars qw($VERSION @EXPORT_OK);
 
-use Class::Inspector;
-
-
 $VERSION = '0.3';
 @EXPORT_OK = qw(list_packages);
 
@@ -74,11 +71,39 @@ sub list_packages {
                 $cand =~ s!::$!!;
                 my @children = list_packages($pack.$cand);
     
-                push @packs, "$pack$cand" unless $cand =~ /^::/ || !Class::Inspector->loaded($pack.$cand); # or @children;
+                push @packs, "$pack$cand" unless $cand =~ /^::/ ||
+                    !__PACKAGE__->_loaded($pack.$cand); # or @children;
                 push @packs, @children;
             }
             return grep {$_ !~ /::::ISA::CACHE/} @packs;
 }
+
+### XXX this is an inlining of the Class-Inspector->loaded()
+### method, but inlined to remove the dependency.
+sub _loaded {
+       my ($class, $name) = @_;
+
+    no strict 'refs';
+
+       # Handle by far the two most common cases
+       # This is very fast and handles 99% of cases.
+       return 1 if defined ${"${name}::VERSION"};
+       return 1 if defined @{"${name}::ISA"};
+
+       # Are there any symbol table entries other than other namespaces
+       foreach ( keys %{"${name}::"} ) {
+               next if substr($_, -2, 2) eq '::';
+               return 1 if defined &{"${name}::$_"};
+       }
+
+       # No functions, and it doesn't have a version, and isn't anything.
+       # As an absolute last resort, check for an entry in %INC
+       my $filename = join( '/', split /(?:'|::)/, $name ) . '.pm';
+       return 1 if defined $INC{$filename};
+
+       '';
+}
+
 
 =head1 AUTHOR
 
